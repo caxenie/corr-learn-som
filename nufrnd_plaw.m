ee0ec5744b6c44dc9da8752345a0669537c89f8b
Cristian Axenie <cristian.axenie@gmail.com>
2014-09-23 16:47:17 +0200

    Reworked sensory data generator and visualization. Optimized for 4 non-uniform distributions types. Stable version.

--- nufrnd_plaw.m
+++
@@ -1,9 +0,0 @@
-% function to generate non-uniform distributed values in a given interval
-% [vmin, vmax] for a powerlaw function
-function y = nufrnd_plaw(x, vmin, vmax, expn)
-    if(expn==-1)
-            y  = vmin*exp(log(vmax - vmin).*x);
-    else
-            y  = exp(log(x.*(-vmin^(expn+1) + vmax^(expn+1)) + vmin^(expn+1))/(expn+1));
-    end
-end
